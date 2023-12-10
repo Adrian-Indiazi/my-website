0e6498848d99fce5495f0e44fe2371c9a4b5c8c5
Adrian-Indiazi <grete084@gmail.com>
2023-12-10 02:22:49 -1100

    made some changes to some components and updated them with accurate data

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -10,33 +10,33 @@ const EmailSection = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+    // const data = {
+    //   email: e.target.email.value,
+    //   subject: e.target.subject.value,
+    //   message: e.target.message.value,
+    // };
+    // const JSONdata = JSON.stringify(data);
+    // const endpoint = "/api/send";
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
+    // // Form the request for sending data to the server.
+    // const options = {
+    //   // The method is POST because we are sending data.
+    //   method: "POST",
+    //   // Tell the server we're sending JSON.
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   // Body of the request is the JSON data we created above.
+    //   body: JSONdata,
+    // };
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
+    // const response = await fetch(endpoint, options);
+    // const resData = await response.json();
 
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
+    // if (response.status === 200) {
+    //   console.log("Message sent.");
+    //   setEmailSubmitted(true);
+    // }
   };
 
   return (
